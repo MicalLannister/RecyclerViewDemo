e694e54d5032d37ad94a8e36d70c98f5d33f652e
lym <caiptain@gmail.com>
2019-09-25 18:59:36 +0800

    add

---
+++ app/src/main/java/com/cq/lannister/recyclerviewdome/itemdecoration/GroupDecoration.java
@@ -0,0 +1,36 @@
+package com.cq.lannister.recyclerviewdome.itemdecoration;
+
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+/**
+ * create by lym on 2019/9/24.
+ */
+public class GroupDecoration extends RecyclerView.ItemDecoration  {
+
+    private List<? extends IGroupItem> gridItems;
+
+    private int titleHeight;
+
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+    }
+
+    @Override
+    public void onDraw(@NonNull Canvas c, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        super.onDraw(c, parent, state);
+    }
+
+    @Override
+    public void onDrawOver(@NonNull Canvas c, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        super.onDrawOver(c, parent, state);
+    }
+
+}
---
+++ app/src/main/java/com/cq/lannister/recyclerviewdome/itemdecoration/IGroupItem.java
@@ -0,0 +1,9 @@
+package com.cq.lannister.recyclerviewdome.itemdecoration;
+
+/**
+ * create by lym on 2019/9/24.
+ */
+public interface IGroupItem {
+    boolean isShow();
+    String getTag();
+}
